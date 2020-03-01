ff0900bbac7e4e75f2fe38eb1cd864c699e05929
J. Morio Sakaguchi <morio.earth@gmail.com>
2020-02-29 20:06:03 -0800

    Ready for graphql to be worked on

--- index.js
+++ index.js
@@ -5,18 +5,19 @@ import express from 'express';
 
 const app = express();
 const staticRoute = express.static('public')
-
-const PORT = process.env.PORT || 8080
 const env = process.env.NODE_ENV || 'development'
+const PORT = process.env.PORT || 8080
 
-app.use('api/graphql', graphqlHTTP({
+app.use('/api/graphql', graphqlHTTP({
     schema,
     rootValue: resolvers,
     graphiql: env === 'development'
 }))
-app.use( staticRoute );
-app.use('/static', staticRoute );
-app.listen(PORT, 'localhost');
+
+app.use( staticRoute )
+app.use('/static', staticRoute )
+app.listen(PORT, 'localhost', () => {console.log(`Server running on http://localhost:${PORT}`)})
+
 
 export default app;
 
